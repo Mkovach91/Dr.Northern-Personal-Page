ba1ca053d55e430293970be41c53497d52596fd5
matt k <matt.kovach91@gmail.com>
2025-02-12 09:51:08 -0600

    css templates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap'
+import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
